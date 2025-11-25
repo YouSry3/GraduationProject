f93063028005a8dbe0735e6aabcf939df2530cc4
YouSry3 <myosre122@gmail.com>
2025-11-25 19:16:04 +0200

    feat:Add Connection String in project

---
+++ Back/GraduationProject/GraduationProject/GlobalDependancyInjection.cs
@@ -0,0 +1,47 @@
+﻿using GraduationProject.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+
+namespace GraduationProject
+{
+    public static class GlobalDependancyInjection
+    {
+        public static IServiceCollection AddProjectServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // DbContext
+            services.AddDbContext<AppDbContext>(options =>
+                // Use SQL Server                                    ============= Path Connection in appSettings.json file ==============
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            var AllowsOrigin = configuration.GetSection("AllowedOrigin").Get<string[]>();
+
+            services.AddCors(options =>
+                   options.AddPolicy("AllowAll", builder =>
+                   builder
+                   .AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .WithOrigins(AllowsOrigin!)
+                   ));
+
+            //Custom Allow Origin for specific client
+            //.WithOrigins("http://localhost:5173")
+
+
+            // Controllers + FluentValidation
+            services.AddControllers();
+
+
+
+            // Swagger
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+       
+       
+    }
+}
---
+++ Back/GraduationProject/GraduationProject/Program.cs
@@ -0,0 +1,41 @@
+﻿// مهم عشان AddProjectServices تشتغل (Namespace بتاعك)
+
+namespace GraduationProject;
+using GraduationProject;
+
+public class Program
+    {
+    public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            //builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+            //    .AddEntityFrameworkStores<AppDbContext>();
+
+
+            //(Controllers + FluentValidation + Mapster + Swagger)
+            builder.Services.AddProjectServices(builder.Configuration);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            app.UseAuthorization();
+
+            //app.MapIdentityApi<ApplicationUser>();
+
+            app.MapControllers();
+
+
+            app.Run();
+        }
+    }
+
